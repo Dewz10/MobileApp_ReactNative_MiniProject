5c6b4cfa46b308c4b2e59d925746f27100f74ca9
autsadawut.kl <autsadawut.dew10@gmail.com>
2023-04-05 18:52:46 +0700

    update

--- 6320500719-quiz-app/src/components/shared/FormInput.js
+++ 6320500719-quiz-app/src/components/shared/FormInput.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { COLORS } from "../../constants/theme";
+import { TextInput } from "react-native-gesture-handler";
 
 const FormInput = ({
   lableText = "",
@@ -18,7 +19,13 @@ const FormInput = ({
           borderColor: COLORS.black,
           borderWidth: 1,
           width: "100%",
+          borderRadius: 5,
+          marginTop: 10,
         }}
+        placeholder={placeholderText}
+        onChangeText={onChangeText}
+        value={value}
+        {...more}
       ></TextInput>
     </View>
   );
--- 6320500719-quiz-app/src/screens/SignInScreen.js
+++ 6320500719-quiz-app/src/screens/SignInScreen.js
@@ -2,12 +2,19 @@ import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { COLORS } from "../constants/theme";
+import FormInput from "../components/shared/FormInput";
+import FormButton from "../components/shared/FormButton";
+import signIn from "../Utils/auth";
 
-const SignInScreen = () => {
+const SignInScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleOnSubmit = () => {};
+  const handleOnSubmit = () => {
+    if (email != "" && password != "") {
+      signIn(email, password);
+    }
+  };
 
   return (
     <SafeAreaView
@@ -29,7 +36,37 @@ const SignInScreen = () => {
       >
         Sign In
       </Text>
-      <FormInput lableText="Email" placeholder="enter your email" />
+      <FormInput
+        lableText="Email"
+        placeholder="enter your email"
+        onChangeText={(value) => setEmail(value)}
+        value={email}
+        keyboardType={"email-address"}
+      />
+
+      <FormInput
+        lableText="Password"
+        placeholder="enter your password"
+        onChangeText={(value) => setPassword(value)}
+        value={password}
+        secureTextEntry={true}
+      />
+      <FormButton
+        lableText="Submit"
+        handleOnPress={handleOnSubmit}
+        style={{ width: "100%" }}
+      />
+      <View
+        style={{ flexDirection: "row", alignItems: "center", marginTop: 9 }}
+      >
+        <Text>Don't have an account?</Text>
+        <Text
+          style={{ marginLeft: 4, color: COLORS.primary }}
+          onPress={() => navigation.navigate("SignUpScreen")}
+        >
+          Create account
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
